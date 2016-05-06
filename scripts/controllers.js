65ec9dda33e455e79765af6b2a3771cc52aed736
Mariot Tsitoara <mariot.tsitoara@gmail.com>
2016-05-06 09:51:11 +0300

    Initial commit

---
+++ scripts/controllers.js
@@ -0,0 +1,27 @@
+var myApp = angular.module('myApp', []);
+
+myApp.controller('MyController', ['$scope', '$http', function($scope, $http) {
+	$scope.showPlayButton = true;
+	$scope.showLaunchButton = false;
+
+	$scope.startGame = function() {
+		$scope.restartGame();
+		$scope.showLaunchButton = true;
+		$scope.showPlayButton = false;
+	}
+
+	$scope.restartGame = function() {
+		var dataToGet = 'data/emptygamedata.json';
+		$http.get(dataToGet).success(function(dataReceived) {
+			$scope.gameData = dataReceived;
+		});
+	}
+
+	$scope.getGameDataFromServer = function(frameNumber) {
+		console.log(frameNumber);
+		var dataToGet = 'data/gamedataframe' + frameNumber + '.json';
+		$http.get(dataToGet).success(function(dataReceived) {
+			$scope.gameData = dataReceived;
+		});
+	}
+}]);
